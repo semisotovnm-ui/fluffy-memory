afec64d279032f2662214cc09e23fb808a48ee6a
SEMISOTOV NIKITA <semisotovnm@gmail.com>
2025-09-17 03:32:07 +0700

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,7 @@
+distance_1 = int(input("Введите километры: "))
+distance_2 = int(input("Введите метры: "))
+distance_1 = distance_1*1000
+if distance_1>distance_2:
+    print(distance_2, 'метров)')
+else:
+    print(distance_1, 'метров)')
