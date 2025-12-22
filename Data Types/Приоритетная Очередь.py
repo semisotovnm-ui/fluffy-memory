163e3db44a23b3ce1143f9a876b991235810ed8b
SEMISOTOV NIKITA <semisotovnm@gmail.com>
2025-12-23 03:55:28 +0700

    Update Приоритетная Очередь.py

--- Data Types/Приоритетная Очередь.py
+++ Data Types/Приоритетная Очередь.py
@@ -1,96 +1,44 @@
-class HashMap:
-    def __init__(self, size=10):
-        self.size = size
-        self.slots = [None] * size
-        self.data = [None] * size
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
 
-    def _hash(self, key):
-        if isinstance(key, str):
-            hash_value = sum(ord(char) for char in key)
-        else:
-            hash_value = hash(key)
-        return hash_value % self.size
+    def is_empty(self):
+        return len(self.elements) == 0
 
-    def put(self, key, value):
-        hash_value = self._hash(key)
-        if self.slots[hash_value] is None:
-            self.slots[hash_value] = key
-            self.data[hash_value] = value
-        elif self.slots[hash_value] == key:
-            self.data[hash_value] = value
-        else:
-            next_slot = (hash_value + 1) % self.size
-            while self.slots[next_slot] is not None and self.slots[next_slot] != key:
-                next_slot = (next_slot + 1) % self.size
+    def push(self, item, priority):
+        self.elements.append((item, priority))
 
-            if self.slots[next_slot] is None:
-                self.slots[next_slot] = key
-                self.data[next_slot] = value
-            else:
-                self.data[next_slot] = value
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("pop from empty priority queue")
+        priority_index = 0
+        for i in range(1, len(self.elements)):
+            if self.elements[i][1] < self.elements[priority_index][1]:
+                highest_priority_index = i
+        item, _ = self.elements.pop(priority_index)
+        return item
 
-    def get(self, key, default=None):
-        start_hash = self._hash(key)
-        position = start_hash
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("peek from empty priority queue")
+        priority_item = self.elements[0][0]
+        priority2 = self.elements[0][1]
+        for item, priority in self.elements:
+            if priority < priority2:
+                priority2 = priority
+                priority_item = item
+        return priority_item
 
-        while self.slots[position] is not None:
-            if self.slots[position] == key:
-                return self.data[position]
-            position = (position + 1) % self.size
-            if position == start_hash:
-                break
-        return default
+    def size(self):
+        return len(self.elements)
 
-    def remove(self, key):
-        start_hash = self._hash(key)
-        position = start_hash
+pq = PriorityQueue()
 
-        while self.slots[position] is not None:
-            if self.slots[position] == key:
-                self.slots[position] = None
-                self.data[position] = None
-                self._rehash_after_remove(position)
-                return
-            position = (position + 1) % self.size
-            if position == start_hash:
-                break
+pq.push("Задача 1", priority=2)
+pq.push("Задача 2", priority=5)
+pq.push("Задача 3", priority=1)
 
-    def _rehash_after_remove(self, removed_index):
-        current = (removed_index + 1) % self.size
-        while self.slots[current] is not None:
-            key_to_reinsert = self.slots[current]
-            value_to_reinsert = self.data[current]
-            self.slots[current] = None
-            self.data[current] = None
-            self.put(key_to_reinsert, value_to_reinsert)
+print(pq.peek())
 
-            current = (current + 1) % self.size
-
-    def keys(self):
-        return [key for key in self.slots if key is not None]
-
-    def values(self):
-        return [value for value in self.data if value is not None]
-
-    def items(self):
-        items_list = []
-        for i in range(self.size):
-            if self.slots[i] is not None:
-                items_list.append((self.slots[i], self.data[i]))
-        return items_list
-
-
-my_hashmap = HashMap()
-my_hashmap.put("name", "John")
-my_hashmap.put("age", 25)
-my_hashmap.put("city", "Example City")
-
-print("Keys:", my_hashmap.keys())  
-print("Values:", my_hashmap.values()) 
-print("Items:", my_hashmap.items()) 
-
-print("Name:", my_hashmap.get("name")) 
-print("Gender:", my_hashmap.get("gender", "Not specified"))
-
-my_hashmap.remove("age")
-print("Keys after removing 'age':", my_hashmap.keys()) 
+while not pq.is_empty():
+    print(pq.pop())
